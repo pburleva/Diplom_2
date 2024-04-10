b4c43306982ec1b82d090a2809b2cc06c5648011
Polina <polinaantonovna@yandex.com>
2024-04-10 20:56:11 +0700

    Added 4 tests after review: Added tests with parsed result of created order

--- tests/create_order_test.py
+++ tests/create_order_test.py
@@ -10,6 +10,30 @@ class TestCreateOrder:
         response = helper_order.create_order(user_fixture[3], data.INGREDIENTS)
         assert response.status_code == 200
 
+    @allure.title('Создание заказа и проверка имени')
+    @allure.description('Проверяем что создан заказ с ожидаемым именем')
+    def test_name_of_created_order_successful(self, user_fixture):
+        response = helper_order.create_order(user_fixture[3], data.INGREDIENTS)
+        assert response.json()['name'] == "Био-марсианский метеоритный бургер"
+
+    @allure.title('Создание заказа и проверка успешности')
+    @allure.description('Проверяем что создан заказ успешно Success параметр = true')
+    def test_success_parameter_of_created_order_successful(self, user_fixture):
+        response = helper_order.create_order(user_fixture[3], data.INGREDIENTS)
+        assert response.json()['success'] == True
+
+    @allure.title('Создание заказа и проверяем что number есть')
+    @allure.description('Проверяем что у заказа есть параметр number')
+    def test_number_parameter_of_created_order_successful(self, user_fixture):
+        response = helper_order.create_order(user_fixture[3], data.INGREDIENTS)
+        assert 'number' in response.json()['order']
+
+    @allure.title('Создание заказа и проверяем что number не пуст')
+    @allure.description('Проверяем что создан заказ и параметр number не пустой')
+    def test_success_parameter_of_created_order_successful(self, user_fixture):
+        response = helper_order.create_order(user_fixture[3], data.INGREDIENTS)
+        assert response.json()['order']['number'] != ''
+
     @allure.title('Создание заказа с ингридиентами пользователем без авторизации')
     @allure.description('Проверяем что создание заказа вернет код 200 для пользователя без авторизации')
     def test_create_order_without_authorization_successful(self):
